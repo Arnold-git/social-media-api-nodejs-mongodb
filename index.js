094b9a68b11a173215a1b868430bb5100e0277bf
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-08-04 12:11:02 +0100

    db connected

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true},
+     ()=>{
+    console.log("Database connected");
+});
 app.listen(8080, ()=>{
     console.log("Backend server is running")
 });
