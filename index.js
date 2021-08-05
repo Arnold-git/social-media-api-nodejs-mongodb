f697dae2ad2717cf1b6ed028640b76a8e16217e5
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-08-05 17:22:45 +0100

    db test

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const userRoute = require('./routes/users')
+const authRoute = require('./routes/auth')
 
 dotenv.config();
 
@@ -26,6 +27,8 @@ app.use(morgan('common'));
 
 
 app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+
 
 
 app.listen(8080, ()=>{
---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const User = require('./models/User');
+
+// Register
+
+
+router.get("/register", async (req, res) => {
+    const user = await new User({
+        username: "John",
+        email:"john@gmail.com",
+        password: "123456"
+
+    })
+
+    await user.save();
+    res.send('ok')
+});
+
+
+
+module.exports = router
