965464d2e95076799b09e5af8219888c5822ddd0
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-08-09 22:39:06 +0100

    some updates

--- routes/users.js
+++ routes/users.js
@@ -3,32 +3,36 @@ const router = require('express').Router();
 const bcrypt = require("bcrypt");
 
 // update user
-router.put("/:id", async (req, res)=>{
-    if(req.body.userId === req.params.id || req.userId.isAdmin){
+router.put("/:id", async(req, res)=>{
+    if(req.body.userId === req.params.id || req.user.isAdmin) {
         if(req.body.password){
             try{
                 const salt = await bcrypt.genSalt(10);
                 req.body.password = await bcrypt.hash(req.body.password, salt);
-
-            } catch (err) {
+            }   catch (err) {
                 return res.status(500).json(err);
             }
         }
         try {
-            const user = await User.findByIdAndUpdate(req.params.id, {
+            const user = await User.findByIdAndUpdate(req.params.id,{
                 $set: req.body,
             });
             res.status(200).json("Account has been updated")
-        } catch (err) {
+        }   catch (err) {
             return res.status(500).json(err);
         }
-
-    } else {
+    }   else {
         return res.status(403).json("You can update only your account")
-    }
-})
+    }  
+
+});
+
 
 // delete user
+
+
+
+
 // get a user
 // follow user
 // unfollow user
