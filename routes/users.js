9b306ef6529b14898e49605aac697b083ec069cf
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-08-10 20:44:20 +0100

    delete user route user

--- routes/users.js
+++ routes/users.js
@@ -28,6 +28,22 @@ router.put("/:id", async (req, res) => {
     }
   });
 
+// delete user
+
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+
+    try {
+      const user = await User.deleteOne(req.params.id); 
+      res.status(200).json("Account has been deleted successfully");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("You can delete only your account!");
+  }
+});
+
 
 // get a user
 // follow user
