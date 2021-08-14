056ca04f5857a293255c47b8bf0f79e8d31ca066
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-08-14 22:44:58 +0100

    :memo get post route added

--- routes/posts.js
+++ routes/posts.js
@@ -65,7 +65,14 @@ router.put("/:id/like", async (req, res)=>{
 
 // get a post
 
-router.get("/:id", async)
+router.get("/:id", async(req, res)=>{
+    try{
+        const post = await Post.findById(req.params.id)
+        res.status(200).json(post);
+    }catch {
+        res.status(500).json(err)
+    }
+});
 
 
 
