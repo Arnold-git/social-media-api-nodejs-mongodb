778f73ac9fa8aa361a2d4ec98e6b4c9bbe229e43
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-08-14 17:51:44 +0100

    :memo: update post route updated

--- routes/posts.js
+++ routes/posts.js
@@ -14,13 +14,18 @@ router.post("/", async(req, res) =>{
 
 // update a post 
 router.put("/:id", async(req, res)=> {
-    const post = Post.findById(req.params.id);
-    if(post.userId === req.body.userId){
-
-    }else{
-        res.status(403).json("You can update only your post")
+    try{
+        const post = Post.findById(req.params.id);
+        if(post.userId === req.body.userId){
+            await post.updateOne({ $set:req.body });
+            res.status(200).json("Your post has beeen updates")
+    
+        }else{
+            res.status(403).json("You can update only your post")
+        }
+    }catch (err) {
+        res.status(500).json(err);
     }
-
 });
 
 module.exports = router;
