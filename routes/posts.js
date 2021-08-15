1a83e8bb862cb5e26b153639e00e5b93996fe9bc
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-08-15 17:18:09 +0100

    :memo: get post timeline added :fire:

--- routes/posts.js
+++ routes/posts.js
@@ -74,6 +74,22 @@ router.get("/:id", async(req, res)=>{
     }
 });
 
+// get timeline post
+
+router.get("/timeline", async(req, res)=>{
+    try{
+        const currentUser = await User.findById(req.body.userId);
+        const userPosts = await Post.find({ userId: currentUser._id });
+        const friendPosts = await Promise.all(
+            current.followings.map(friendId=>{
+                Post.find({ userId: friendId });
+            })
+        );
+        res.json(userPosts.concat(...friendPosts))
+    }   catch(err){
+        res.status(500).json(err)
+    }
+});
 
 
 module.exports = router;
